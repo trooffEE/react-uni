ed494d43d917f803d2ff09ecae429ba299853086
Vadim <spicyn.v2001@gmail.com>
2021-05-23 17:48:19 +0700

    5 таска state

--- src/TasksState/TaskState6.jsx
+++ src/TasksState/TaskState6.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const TaskState6 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <ShowMoreWithText
+      listOfText={[
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          soluta asperiores itaque.
+        </p>,
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>,
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui officiis
+          unde quis quas?
+        </p>,
+      ]}
+    />
+  );
+};
 
+const ShowMoreWithText = ({ listOfText }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <>
+      {listOfText.map((item, idx) => {
+        if (expanded) {
+          return item;
+        } else if (idx !== 0) {
+          return;
+        }
+        return item;
+      })}
+      <button onClick={() => setExpanded(!expanded)}>
+        {expanded ? "Скрыть" : "Показать все"}
+      </button>
+    </>
+  );
+};
 
 export default TaskState6;
